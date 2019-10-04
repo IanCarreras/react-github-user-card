0c563b539a7237304a013ae444aec490ed4f1a05
Ian <mus5002@aol.com>
2019-10-03 21:18:42 -0400

    added component files, fetching data on app mount and setting to state

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React from 'react'
+import axios from 'axios'
 
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  state = {}
 
-    </div>
-  );
+  componentDidMount() {
+    axios.get('https://api.github.com/users/iancarreras')
+      .then(res => {
+        return this.setState({userData: res.data})
+      })
+      .catch(err => {
+        return err.response
+      })
+
+    axios.get('https://api.github.com/users/iancarreras/followers')
+      .then(res => {
+        this.setState({...this.state, followers: res.data})
+      })
+      .catch(err => {
+        return err.response
+      })
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App">
+        User Card
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
